96eb387a0ecf71057ff3f3f5b33484dae1ae9677
Daniel Prevoznik <dprevoznik@gmail.com>
2020-01-27 08:57:15 -0500

    "Completed eliminate consecutive duplicates exercise. All tests currently passing."

--- src/recursion.js
+++ src/recursion.js
@@ -475,6 +475,12 @@ var letterTally = function(str, obj) {
 // compress([1,2,2,3,4,4,5,5,5]) // [1,2,3,4,5]
 // compress([1,2,2,3,4,4,2,5,5,5,4,4]) // [1,2,3,4,2,5,4]
 var compress = function(list) {
+  if ( list.length === 1 ) { return list; }
+
+  var recursionList = compress(list.slice(1));  
+  if ( list[0] !== list[1] ) { recursionList.unshift(list[0]); }
+
+  return recursionList;
 };
 
 // 33. Augment every element in a list with a new value where each element is an array
